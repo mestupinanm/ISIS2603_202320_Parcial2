3cf355a376724e53e92818a764435ced63611f0c
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2023-11-24 09:05:51 -0800

    cambios que hacen que este mejor

--- src/app/Receta/Receta.ts
+++ src/app/Receta/Receta.ts
@@ -1,22 +1,40 @@
 export class Receta {
+    id: number;
+    nombre: string;
+    descripcion: string; 
+    dificultad: string;
+    duracion: number;
+    ingredientes: string;
+    categoria: string;
+    imagen: string; 
+    estrellas: number; 
+    cantidadOpiniones: number; 
+    autorReceta: string; 
   
-    id: number;                
-    nombre: string;           
-    imagenUrl: string;       
-    calificacion: number;      
-    numOpiniones: number;      
-    duracion: number;          
-    dificultad: string;        
-    autor: string;            
-
-    public constructor(id: number,nombre: string,imagenUrl: string,calificacion: number,numOpiniones: number,duracion: number,dificultad: string,autor: string) {
-        this.id = id;
-        this.nombre = nombre;
-        this.imagenUrl = imagenUrl;
-        this.calificacion = calificacion;
-        this.numOpiniones = numOpiniones;
-        this.duracion = duracion;
-        this.dificultad = dificultad;
-        this.autor = autor;
+    public constructor(
+      id: number,
+      nombre: string,
+      descripcion: string,
+      dificultad: string,
+      duracion: number,
+      ingredientes: string,
+      categoria: string,
+      imagen: string,
+      estrellas: number,
+      cantidadOpiniones: number,
+      autorReceta: string
+    ) {
+      this.id = id;
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+      this.dificultad = dificultad;
+      this.duracion = duracion;
+      this.ingredientes = ingredientes;
+      this.categoria = categoria;
+      this.imagen = imagen;
+      this.estrellas = estrellas;
+      this.cantidadOpiniones = cantidadOpiniones;
+      this.autorReceta = autorReceta;
     }
-}
+  }
+  
