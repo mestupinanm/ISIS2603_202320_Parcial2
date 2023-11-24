bfb8ffe8e9891c38d7220092183760ffa9a30a66
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2023-11-24 08:56:21 -0800

    SIRVE URL

--- src/app/Receta/Receta.service.ts
+++ src/app/Receta/Receta.service.ts
@@ -8,7 +8,7 @@ import { Receta } from './Receta';
 })
 export class RecetaService {
 
-  private apiUrl = 'https://gist.githubusercontent.com/VivianGomez/e65c117a4809355fbb53c37cb8fe1d44/raw/ d0482c083962cad2319b3640250da0194d341dc6/recipes'; // Reemplazar con la URL de tu API
+  private apiUrl = 'https://gist.githubusercontent.com/VivianGomez/e65c117a4809355fbb53c37cb8fe1d44/raw/d0482c083962cad2319b3640250da0194d341dc6/recipes'; 
 
   constructor(private http: HttpClient) { }
 
